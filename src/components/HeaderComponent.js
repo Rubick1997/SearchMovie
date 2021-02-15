e12e63a9d0243cc57819e6495497d8cf8ea551f2
Rustam <rustam905@gmail.com>
2021-02-15 15:16:51 -0800

    .

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Button, Row, Col, Container } from "reactstrap";
+import { Navbar, Button, Row, Col} from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import {
--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, FormGroup, Label, Input, Button, Container } from "reactstrap";
+import { Form, FormGroup, Label, Input, Button} from "reactstrap";
 import { Link } from "react-router-dom";
 
 function Search() {
