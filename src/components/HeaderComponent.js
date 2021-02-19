cf57bc558733970ba7e0f41f2a72c5ea0977075f
Rustam <rustam905@gmail.com>
2021-02-18 16:16:54 -0800

    App is ready

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,17 @@
-import React from "react";
-import { Navbar, Button, Row, Col} from "reactstrap";
+import React, { useState } from "react";
+import {
+	Navbar,
+	Button,
+	Row,
+	Col,
+	Modal,
+	ModalHeader,
+	ModalBody,
+	Form,
+	FormGroup,
+	Label,
+	Input,
+} from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import {
@@ -9,29 +21,24 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
+	const [modal, setModal] = useState(false);
+	const [modal2,setModal2] = useState(false)
+
+	const toggle = () => setModal(!modal);
+	const toggle2 = () => setModal2(!modal2)
+
+
 	return (
 		<React.Fragment>
-			<Navbar dark expand='md'>
-				<span className='navbar-text ml-auto'>
-					<ul className='navbar-nav ml-auto mt-2'>
-						<Button className='button' outline>
-							<FontAwesomeIcon icon={faSignInAlt} /> Login
-						</Button>
-						<Button className='button' outline>
-							<FontAwesomeIcon icon={faUserPlus} /> Register
-						</Button>
-					</ul>
-				</span>
-			</Navbar>
-				<Row>
-					<Col>
-						<h1 className='title'>
-							<Link to='/' style={{ color: "black", textDecoration: "none" }}>
-								Movie App <FontAwesomeIcon icon={faFilm} />
-							</Link>
-						</h1>
-					</Col>
-				</Row>
+			<Row>
+				<Col>
+					<h1 className='title'>
+						<Link to='/' style={{ color: "black", textDecoration: "none" }}>
+							Movie App <FontAwesomeIcon icon={faFilm} />
+						</Link>
+					</h1>
+				</Col>
+			</Row>
 		</React.Fragment>
 	);
 }
